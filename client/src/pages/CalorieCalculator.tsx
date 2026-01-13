9bad8ef4a2ce5db655f79b8bb269fc2a4f09fc8d
MohAli92 <mohamed.nouh@stud.th-deg.de>
2026-01-13 13:39:10 +0100

    second commit

--- client/src/pages/CalorieCalculator.tsx
+++ client/src/pages/CalorieCalculator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
 import { CalculatorIcon, SparklesIcon } from '@heroicons/react/24/outline';
